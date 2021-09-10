63c5b9bf6b58165625b0c9c38cbe9c37601c71f3
Hydroxycarbamide <nguyen.huang.eric@gmail.com>
2021-09-10 17:10:06 +0200

    Set the video container draggable

--- AMQ-UI-Rearranger.js
+++ AMQ-UI-Rearranger.js
@@ -627,9 +627,6 @@ function StartGameCallback() {
         quiz.scoreboard.setActiveGroup("1")
         quiz.avatarContainer.currentGroup = 1
     }, 30000);
-    setTimeout(function(){
-        createDragableDiv();
-    })
 };
 
 function SetupMirrorTimer() {
@@ -667,6 +664,7 @@ function StartAmqScript() {
         SetupMirrorTimer();
         AddSecondarySongInfo();
         AddSongDataHolder();
+        SetDraggableVideo();
 
         new Listener('Game Starting', function () {
             StartGameCallback();
@@ -695,55 +693,6 @@ new Listener('Spectate Game', function () {
     StartAmqScript();
 }).bindListener();
 
-
-function createDragableDiv() {
-    const videoContainer = document.getElementById("qpVideoContainer");
-    const dragableElement = document.createElement("div");
-    dragableElement.id = 'qpVideoContainerHeader';
-    videoContainer.insertBefore(dragableElement, videoContainer.firstChild);
-    dragElement(videoContainer);
-}
-
-//Make the DIV element draggagle:
-
-
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+function SetDraggableVideo() {
+    $('#qpVideoContainer').draggable();
 }
